b7a7edce0e77bd340673583b029cc1cf54ce2083
Namrata <namratagupta1011@gmail.com>
2017-09-28 20:20:23 +0530

    knock in file02

--- file02.js
+++ file02.js
@@ -1,6 +1,8 @@
 
 funtion greet(name) {
-	console.log('Hi! '+name);
+	console.log('Knock Knock! '+name)
+	console.log('Knock Knock! '+name)
+	console.log('Knock Knock! '+name)
 }
 
 greet('Namrata');
